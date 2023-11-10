9ed4e142565a5d81d9caf62e9d38d5e3d63ef50f
Mark Donatelli <markdonatelli@Marks-MacBook-Pro.local>
2023-11-10 13:47:26 -0500

    nuxt image module added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/scss/main.scss'],
-  devtools: { enabled: false }
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_colors.scss" as *;'
+        }
+      }
+    }
+  },
+
+  devtools: { enabled: false },
+  modules: ["@nuxt/image"]
 });
