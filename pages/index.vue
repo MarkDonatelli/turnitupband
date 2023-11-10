d6a40c4d729863c7e3e1ed1c73b6a72c03b33af2
Mark Donatelli <markdonatelli@Marks-MacBook-Pro.local>
2023-11-10 13:23:42 -0500

    scss added to project

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <Head><Title>Thrash Party Band | Boston, MA</Title></Head>
+  <div class="main">
+    <div>
+      <h1>THRASH PARTY BAND</h1>
+      <a href="https://www.youtube.com/watch?v=WccfbPQNMbg" target="_blank">
+        hmmm...
+      </a>
+    </div>
+    <p>Coming Soon...&#129304;</p>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
