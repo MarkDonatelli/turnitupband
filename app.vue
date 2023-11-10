3314557dc8eeb94b8d982dec2c122aa2bc947f86
Mark Donatelli <markdonatelli@Marks-MacBook-Pro.local>
2023-11-10 11:15:20 -0500

    testing branches

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <Head><Title>Thrash Party Band | Boston, MA</Title></Head>
-  <div>THRASH PARTY</div>
+  <div>THRASH PARTY BAND</div>
   <a href="https://www.youtube.com/watch?v=WccfbPQNMbg" target="_blank">
     hmmm...
   </a>
