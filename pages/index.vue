1cd5d75e7cc525b6c9dc5250a515b086b2ed84dd
Mark Donatelli <markdonatelli@Marks-MacBook-Pro.local>
2024-01-22 14:59:04 -0500

    test form

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,7 @@
 <script setup>
   const form = ref({
-    name: '',
-    email: '',
-    message: ''
+    name: ''
   });
-
-  async function handleSubmit() {
-    const formData = new FormData();
-    for (let key in form.value) {
-      formData.append(key, form.value[key]);
-    }
-
-    try {
-      const response = await fetch('/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams(formData).toString()
-      });
-
-      if (response.ok) {
-        // Form submission successful
-      } else {
-        // Handle error
-      }
-    } catch (error) {
-      // Handle error
-    }
-  }
 </script>
 
 <template>
@@ -40,7 +13,6 @@
     <p>Coming Soon...&#129304;</p>
 
     <form
-      @submit.prevent="handleSubmit"
       name="contact"
       action=""
       method="POST"
@@ -48,8 +20,7 @@
       data-netlify-honeypot="bot-field"
     >
       <input type="text" v-model="form.name" placeholder="Name" />
-      <input type="email" v-model="form.email" placeholder="Email" />
-      <textarea v-model="form.message" placeholder="Message"></textarea>
+
       <button type="submit">Send</button>
     </form>
   </div>
