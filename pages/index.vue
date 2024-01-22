3f44304234ac8f4d30901055a8cf2fa7e49ab358
Mark Donatelli <markdonatelli@Marks-MacBook-Pro.local>
2024-01-22 14:36:53 -0500

    testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+  const contact_name = ref(null);
+</script>
 
 <template>
   <Head><Title>Turn It Up | Boston, MA</Title></Head>
@@ -7,6 +9,17 @@
       <h1>Turn It Up!</h1>
     </div>
     <p>Coming Soon...&#129304;</p>
+
+    <form name="contactForm" method="post" netlify-honeypot="bot-field" netlify>
+      <input type="hidden" name="form-name" value="contactForm" />
+      <input
+        v-model="contact_name"
+        name="name"
+        type="text"
+        placeholder="Name"
+      />
+      <button type="submit" class="btn btn-primary">Send Message</button>
+    </form>
   </div>
 </template>
 
