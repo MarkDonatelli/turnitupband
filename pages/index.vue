cf966417efb6541ad8f2409ba2de8b4624602e75
Mark Donatelli <markdonatelli@Marks-MacBook-Pro.local>
2024-07-30 17:21:10 -0400

    starting new home section

--- pages/index.vue
+++ pages/index.vue
@@ -8,32 +8,11 @@
 </script>
 
 <template>
-  <div id="home" class="grid md:h-screen md:pt-[57px] scroll-mt-[95px]">
-    <div>
-      <h1
-        class="title md:pt-[35px] lg:pt-0 md:pb-0 font-bold text-center text-5xl md:text-[105px] text-white uppercase lg:text-[140px] leading-normal xl:text-[175px] whitespace-nowrap font-display tracking-tight"
-      >
-        Ready to
-
-        <span class="relative font-extrabold text-pink">Turn It Up!?</span>
-      </h1>
-    </div>
-    <div class="relative overflow-hidden">
-      <NuxtImg
-        class="absolute top-4 w-[150px] md:w-[200px] right-5 md:right-10 lg:w-[275px] lg:right-16"
-        src="/images/bolt-pink--group.svg"
-        width=""
-        height=""
-        alt=""
-      />
-      <NuxtImg
-        class="object-cover w-full h-full overflow-hidden"
-        src="/images/hero.jpg"
-        width=""
-        height=""
-        alt=""
-      />
-    </div>
+  <div
+    id="home"
+    class="grid md:h-screen place-content-center md:pt-[57px] scroll-mt-[95px]"
+  >
+    <h1 class="text-white">NEW HOME</h1>
   </div>
 
   <!-- videos section -->
